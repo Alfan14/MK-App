8eddf7af7e9e7000f19319745a6d99a2f1fab20e
LAPTOP-CE7UFLHI MINGW64 <septiyan140905@gmail.com>
2024-07-25 10:24:59 +0700

    First Fetch Usign JS

--- Backend/main.js
+++ Backend/main.js
@@ -1,14 +1,15 @@
 window.onload = init;
-const map = new Map({
-    view: new View({
-      center: [0, 0],
-      zoom: 1,
-    }),
-    layers: [
-      new TileLayer({
-        source: new OSM(),
-      }),
-    ],
-    target: 'map',
-  });
-
+function init(){
+    const map = new ol.Map({
+        view: new ol.View({
+            center: [0, 0],
+            zoom: 2
+        }),
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        target: 'js-map'
+    })
+}
